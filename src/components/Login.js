438e53598cb5ecac88533a2c722699c84e730cdf
Brian Walker <88561350+bw01496@users.noreply.github.com>
2021-11-17 23:09:09 -0500

    Update Login.js
    updating from original file

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,48 +1,20 @@
 import React, { useState } from "react";
-import { useParams, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import { callApi } from "../util/api";
 
 const Login = ({ setToken, setUsers }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const params = useParams();
-  const [loggedIn, setLoggedin] = useState(false);
-
-  // function logIn(response){
-  //     if (response.data){
-  //         setToken(response.data.token)
-  //         if(response.data.token == ''){
-  //             setLoggedin(false)
-  //         }else{
-  //             setLoggedin(true)
-  //             navigate('/profile')
-  //         }
-  //     }
-  // }
-
-  // function isLoggedIn(){
-  //         return token === ''
-  // }
-
-  // function logOut(){
-  // Clear token from everywhere and directs them to a page
-  // }
 
   return (
     <>
       <h1>Login</h1>
-      {/* // we need to send a fetch request, so we can get the token
-          // in order to get a token, the server wants... username and password 
-          //When I hit submit it is supposed the username and password from the API. 
-          
-          */}
 
       <form
         onSubmit={async (event) => {
           event.preventDefault();
-          // const URL = (`${APIURL}/users/login`)
-          // console.log(URL)
+
           const respObj = await callApi({
             url: `/users/login`,
             method: "POST",
@@ -53,31 +25,7 @@ const Login = ({ setToken, setUsers }) => {
               },
             },
           });
-          //    try{
-          //     const response = await fetch(URL, {
-          //         method: "POST",
-          //         headers: {
-          //             'Content-Type': 'application/json',
-          //             // 'Authorization': 'Bearer ${token}'
 
-          //         },
-          //         body: JSON.stringify({
-          //             user:{
-          //                 username,
-          //                 password
-          //             }
-          //         })
-          //     })
-          //     const responseObj = await response.json();
-          //     console.log(responseObj)
-          //     return responseObj
-
-          //    } catch (error){
-          //        console.error(error)
-
-          //    }
-          //    console.log(responseObj.data.token)
-          //    console.log(responseObj)
           console.log(respObj);
           if (respObj.data) {
             const userResp = await callApi({
@@ -91,11 +39,6 @@ const Login = ({ setToken, setUsers }) => {
               navigate("/profile");
             }
           }
-          // if(respObj.data.token) {
-
-          // setToken(responseObj.data.token)
-          // navigate('/');
-          // }
         }}
       >
         <input
