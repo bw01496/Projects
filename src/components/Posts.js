b0d105478702ea25a803511335016a47e3af70a2
Brian Walker <88561350+bw01496@users.noreply.github.com>
2021-11-17 23:10:52 -0500

    Update Posts.js
    updating from original file

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -13,60 +13,19 @@ const Posts = ({ posts, token, fetchPosts }) => {
     console.log("respObj: ", respObj);
     await fetchPosts();
   };
-  // const handleEdit = async (postId) => {
-  //   console.log("url: ", `/posts/${postId}`);
-  //   const respObj = await callApi({
-  //     method: "PATCH",
-  //     url: `/posts/${postId}`,
-  //     token,
-  //     body: {
-  //       // post: {
-  //       //   title,
-  //       //   description,
-  //       //   price,
-  //       //   location,
-  //       // },
-  //     },
-  //   });
-  //   console.log("respObj: ", respObj);
-  //   await fetchPosts();
-  // };
 
-  // const [postsx, setPosts] = useState([]);
-  // const handleDelete = async (postIdToDelete) => {
-  //   console.log("postIdToDelete ", postIdToDelete);
-  //   const response = await fetch(`${APIURL}/posts/${postIdToDelete}`, {
-  //     method: "DELETE",
-  //     token,
-  //   });
-  //   const data = await response.json();
-  //   console.log("data: ", data);
-  //   if (data) {
-  //     const newPosts = posts.filter((post) => post._id !== postIdToDelete);
-  //     setPosts(newPosts);
-  //   }
-  // };
   return (
     <>
       <h3>
         <u>List of Posts:</u>
       </h3>
       {posts.map((post) => (
-        /* console.log(post)*/
         <div key={post._id}>
           <h1>{post.title}</h1>
           <p>{post.description}</p>
           <p>{post.price}</p>
           <p>{post.location}</p>
-          {/* {token && (
-            <button
-              type="button"
-              className="btn btn-outline-primary"
-              onClick={() => handleEdit(post._id)}
-            >
-              Edit
-            </button>
-          )} */}
+
           {token && (
             <button
               type="button"
